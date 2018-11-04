ecedb9bb3865e3156d8edb308c17a769c47d74b5
matrixmickey <matrixmickey@gmail.com>
2018-11-04 16:45:40 -0500

    400 error on login attempt
    Attempting to call api for the first time but getting 400 error

--- Fletcher/app/src/main/java/com/example/fletcher/LoginActivity.java
+++ Fletcher/app/src/main/java/com/example/fletcher/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.fletcher;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -29,8 +31,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -185,19 +198,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(this, email, password);
             mAuthTask.execute((Void) null);
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+        return true;
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return true;
     }
 
     /**
@@ -296,34 +307,43 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
+        private Context mContext;
+
         private final String mEmail;
         private final String mPassword;
 
-        UserLoginTask(String email, String password) {
+        UserLoginTask(Context context, String email, String password) {
+            mContext = context;
             mEmail = email;
             mPassword = password;
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
+            JSONObject json = new JSONObject();
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
+                json.put(getString(R.string.login_username), mEmail);
+                json.put(getString(R.string.login_password), mPassword);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, getString(R.string.login_url), new JSONObject() {  }, new Response.Listener<JSONObject>() {
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Intent intent = new Intent(mContext, ScrollingActivity.class);
+                        startActivity(intent);
+                    }
+                }, new Response.ErrorListener() {
 
-            // TODO: register the new account here.
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Intent intent = new Intent(mContext, ScrollingActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            MySingleton.getInstance(mContext).addToRequestQueue(jsonObjectRequest);
             return true;
         }
 
